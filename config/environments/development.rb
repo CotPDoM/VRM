656aa728f00702be37038b9cef95e6c24a749ad0
root <root@azza.(none)>
2010-09-12 01:32:57 -0700

    Fixed clear searches to a link with a icon and description

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,5 +20,16 @@ Practise::Application.configure do
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
   
-
+  # Mailer config
+  config.action_mailer.delivery_method = :smtp 
+  config.action_mailer.smtp_settings = {  
+	      :address => "smtp.gmail.com",
+	      :port => 465,  
+  # 	    :domain => 'pccrovers.com',
+	      :user_name => 'nicholas.pearson@pccrover.com',
+	      :password => 'bci3eblack',
+	      :authentication => :plain,
+	      :tls => true,
+	      :enable_starttls_auto => true } 
+  config.action_mailer.raise_delivery_errors = true
 end
