710c78fdc80f13cf348cf0f0ed02c2b4c66e4824
CotPDoM <nicholas.pearson@pccrovers.com>
2010-09-12 14:04:24 -0700

    added seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,100 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+Person.create!(
+  :first_name=>'Nick',
+  :last_name=>'Pearson',
+  :phone_number=>'7782311022',
+  :email=>'nick@pccrovers.com',
+  :postal_code=>'V6H 1N9',
+  :city=>'Vancouver',
+  :role=>'Team 100'
+)
+
+Person.create!(
+  :first_name=>'Bethany',
+  :phone_number=>'6044698785',
+  :postal_code=>'',
+  :city=>'Richmond',
+  :role=>'Leader'
+)
+
+Person.create!(
+  :first_name=>'Rachel',
+  :city=>'West Vancouver',
+  :role=>'Potential'
+)
+
+Person.create!(
+  :first_name=>'Richard',
+  :last_name=>'Toth',
+  :phone_number=>'6049991221',
+  :email=>'Rich@foo.com',
+  :city=>'North Vancouver',
+  :role=>'Potential'
+)
+
+Person.create!(
+  :first_name=>'Teniya',
+  :last_name=>'Booth',
+  :phone_number=>'7782514786',
+  :email=>'Tdot@foo.com',
+  :city=>'Vancouver',
+  :role=>'Potential'
+)
+
+Person.create!(
+  :first_name=>'Kevin',
+  :last_name=>'Li',
+  :phone_number=>'6048893293',
+  :email=>'kevin.li@pccrovers.com',
+  :city=>'Richmond',
+  :role=>'Team 100'
+)
+
+Person.create!(
+  :first_name=>'Christal',
+  :last_name=>'Mercante',
+  :email=>'Happy_puppy@lookatme.com',
+  :role=>'Potential'
+)
+
+Person.create!(
+  :first_name=>'Feliciay',
+  :last_name=>'Day',
+  :city=>'Vancouver',
+  :role=>'Potential'
+)
+
+Person.create!(
+  :first_name=>'Thomas',
+  :last_name=>'Rather',
+  :phone_number=>'7789983562',
+  :city=>'Burnaby',
+  :role=>'Potential'
+)
+
+Person.create!(
+  :first_name=>'Michael',
+  :last_name=>'Demeyer',
+  :email=>'DMic@bar.com',
+  :role=>'Potential'
+)
+
+Person.create!(
+  :first_name=>'Jessica',
+  :last_name=>'Thompson',
+  :phone_number=>'7785417236',
+  :email=>'Jess@foo.com',
+  :postal_code=>'V5R 3E4',
+  :city=>'Burnaby',
+  :role=>'Potential'
+)
+
+Person.create!(
+  :first_name=>'Shannon',
+  :last_name=>'Mickelson',
+  :city=>'Richmond',
+  :role=>'Potential'
+)
